014ebc073033c5c3d8cf5d1a66b5b118757838ad
Hira-Tech-GenAi <160832321+Hira-Tech-GenAi@users.noreply.github.com>
2024-02-26 21:34:00 +0500

    Delete personal_message directory

--- personal_message/personal_message.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-let personName = "";
-personName = prompt("What is your name?") || "";
-if (personName !== null && personName !== "") {
-    alert(`Hello ${personName}, Would you like to learn some Typescript today.`);
-}
-else {
-    alert("You have to fill your name! ");
-}
