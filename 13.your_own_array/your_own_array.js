a54dcedf1ea77ac4766bc0f9abefa0c1311cb173
Hira-Tech-GenAi <hiratech88@gmail.com>
2024-02-29 16:36:09 +0500

    solve-quiz

---
+++ 13.your_own_array/your_own_array.js
@@ -0,0 +1,5 @@
+"use strict";
+let transportation = ["Jet", "Car", "SuperBike", "SpaceShip"];
+for (let i = 0; i < transportation.length; i++) {
+    console.log("I would like to own a " + transportation[i]);
+}
