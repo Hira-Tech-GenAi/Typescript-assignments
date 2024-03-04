e411f200d6a2a8c1e7d04d4f10c67f6702421e95
Hira-Tech-GenAi <160832321+Hira-Tech-GenAi@users.noreply.github.com>
2024-03-04 19:32:00 +0000

    code correction

--- 15_changing_guest_list/changing_guestLi.js
+++ 15_changing_guest_list/changing_guestLi.js
@@ -4,8 +4,6 @@ let absent_Guest = "Quaid-e-Azam";
 let new_Guest = "Bill Gates";
 guest_list[0] = new_Guest;
 for (let i = 0; i < guest_list.length; i++) {
-    console.log("Dear Sir, " +
-        guest_list[i] +
-        ",\n\nIt is our pleasure to invite you in our party.\n\nThank you!\n\n");
+    console.log("Dear Sir, " + guest_list[i] + ",\n\nIt is our pleasure to invite you in our party.\n\nThank you!");
 }
 console.log(`Sir ${absent_Guest} is not coming to the party.`);
--- 15_changing_guest_list/changing_guestLi.ts
+++ 15_changing_guest_list/changing_guestLi.ts
@@ -5,10 +5,6 @@ let new_Guest: string = "Bill Gates";
 guest_list[0] = new_Guest;
 
 for (let i = 0; i < guest_list.length; i++) {
-  console.log(
-    "Dear Sir, " +
-      guest_list[i] +
-      ",\n\nIt is our pleasure to invite you in our party.\n\nThank you!\n\n"
-  );
+  console.log("Dear Sir, " + guest_list[i] + ",\n\nIt is our pleasure to invite you in our party.\n\nThank you!");
 }
 console.log(`Sir ${absent_Guest} is not coming to the party.`);
