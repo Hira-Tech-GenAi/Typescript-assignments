a135461910b696a65df15f834d56ec37f0de7f22
Hira-Tech-GenAi <160832321+Hira-Tech-GenAi@users.noreply.github.com>
2024-03-04 19:01:48 +0000

    Code Correction

--- 4_famous_quote/famous_quote.js
+++ 4_famous_quote/famous_quote.js
@@ -1,3 +1,2 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 console.log("Ralph Waldo Emerson once said, Life is a journey, not a destination.");
--- 4_famous_quote/famous_quote.ts
+++ 4_famous_quote/famous_quote.ts
@@ -1,3 +1,2 @@
-import { toASCII } from "punycode";
 
 console.log("Ralph Waldo Emerson once said, Life is a journey, not a destination.");
