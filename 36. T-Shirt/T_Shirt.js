43dce7c54a24f090213ea2e21c808b1d8fc1bc5a
Hira-Tech-GenAi <160832321+Hira-Tech-GenAi@users.noreply.github.com>
2024-03-04 22:12:23 +0000

    More question solved

--- 36. T-Shirt/T_Shirt.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-//Written by: Taha Ahmed
-// Roll No. : 00298983
-function make_shirt(size, message) {
-    console.log(`Creating a ${size} shirt with the message: "${message}"`);
-}
-make_shirt('large', 'Hello, world!');
--- 36. T-Shirt/T_Shirt.ts
+++
@@ -1,8 +0,0 @@
-//Written by: Taha Ahmed
-// Roll No. : 00298983
-
-function make_shirt(size: string, message: string): void {
-  console.log(`Creating a ${size} shirt with the message: "${message}"`);
-}
-
-make_shirt('large', 'Hello, world!');
