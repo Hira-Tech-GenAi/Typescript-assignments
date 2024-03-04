07c92df7032cfe0dc2f7f34486e7f03771bea61c
Hira-Tech-GenAi <hiratech88@gmail.com>
2024-03-04 22:34:11 +0500

    solve-quiz

---
+++ 21.objects/typescript_objects.ts
@@ -0,0 +1,24 @@
+
+//write a program that creates objects
+
+interface student{
+    fullName: string,
+    fatherName: string,
+    Age: number,
+    rollNo: number,
+    isPass: boolean
+
+
+}
+
+
+let student:  student = {
+    fullName: "Hira Jabeen",
+    fatherName: "Khalid Siddique",
+    Age: 34,
+    rollNo: 12,
+    isPass: true
+
+}
+console.log(student);
+
